1fe7daff5400b05b75491e9e2965b9e680b72868
Sandro Hawke <sandro@hawke.org>
2016-06-28 09:13:27 -0400

    made standard-js

--- fromHTML.js
+++ fromHTML.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const htmlparser = require("htmlparser2")
-const debug = require('debug')('arraydom.fromHTML')
+const htmlparser = require('htmlparser2')
+// const debug = require('debug')('arraydom.fromHTML')
 const d2 = require('debug')('arraydom.fh2')
 const d3 = require('debug')('arraydom.fh3')
 
 function parseDocument (text) {
-  const wrapper = ['document', {} ] 
+  const wrapper = ['document', {}]
   const stack = [ wrapper ]
   let index = 0
   const parser = new htmlparser.Parser({
-	  onopentag: function (name, attribs) {
+    onopentag: function (name, attribs) {
       d3('open', name)
       d2('open', name, index, stack)
       let me = [name, attribs]
@@ -20,9 +20,8 @@ function parseDocument (text) {
       index++
       stack[index] = me
       d2('leaving: ', index, stack)
-	  },
-	  ontext: function(text){
-
+    },
+    ontext: function (text) {
       if (text.match(/^\r?\n *$/)) {
         // Odds are very, very good this is indenting stuff we don't
         // want....   But this is still imperfect.   Make it a flag?
@@ -30,18 +29,18 @@ function parseDocument (text) {
       }
       d3('text', JSON.stringify(text))
       stack[index].push(text)
-	  },
-	  onclosetag: function(name){
+    },
+    onclosetag: function (name) {
       d2('close', name)
       d3('close', name)
       index--
       d2('leaving: ', index, stack)
-	  },
-    oncomment: function(text) {
+    },
+    oncomment: function (text) {
       stack[index].push(['comment', {}, text])
     },
-    onprocessinginstruction: function(name, data) {
-      stack[index].push(['processinginstruction', {name:name}, data])
+    onprocessinginstruction: function (name, data) {
+      stack[index].push(['processinginstruction', {name: name}, data])
     }
   }, {decodeEntities: true})
   parser.write(text)
@@ -51,7 +50,7 @@ function parseDocument (text) {
 }
 
 function moveClassToTag (tree) {
-  const tags = tree[0] || 'div'
+  // const tags = tree[0] || 'div'
   const attrs = tree[1] || {}
   const children = tree.slice(2)
 
@@ -70,7 +69,7 @@ function moveClassToTag (tree) {
     let val = attrs[key]
     let numval = Number(val)
     if (!Number.isNaN(numval)) {
-      if (""+numval === val) {
+      if ('' + numval === val) {
         attrs[key] = numval
       }
     }
@@ -83,7 +82,7 @@ function moveClassToTag (tree) {
 
 function parseElement (text) {
   const document = parseDocument(text)
-  if (document.length != 3) {
+  if (document.length !== 3) {
     throw new Error('parseElement didnt fine exactly one element, found:', JSON.stringify(document.slice(2)))
   }
   return document[2]
